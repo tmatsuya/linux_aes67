f435806a8e96fd59402c4edc6628919df37135d5
Takeshi Matsuya <macchan@sfc.wide.ad.jp>
2023-06-29 01:55:19 +0900

    dsp_server.c: delay echo filter works

--- dsp_server.c
+++ dsp_server.c
@@ -10,6 +10,11 @@
 #include <sys/time.h>
 #include <alsa/asoundlib.h>
 
+#define	SAMPLE_RATE	(48000)
+
+int	history_left [SAMPLE_RATE];
+int	history_right[SAMPLE_RATE];
+int	history_bottom;
 
 int main(int argc, char **argv) {
 	int sock1;
@@ -37,6 +42,12 @@ int main(int argc, char **argv) {
 	}
 
 
+	// Initialize sound history
+	for (i=0; i<SAMPLE_RATE; ++i) {
+		history_left [i] = 0;
+		history_right[i] = 0;
+	}
+	history_bottom = 0;
 
 	// sock1: source packet
 	sock1 = socket(AF_INET, SOCK_DGRAM, 0);
@@ -82,37 +93,74 @@ int main(int argc, char **argv) {
 
 			nokori = rc;
 			if (pcm_byte_per_frame == 4) {
-				short left, right;
+				int left, right, p1, p2, p3, p4, p5, p6;
 				for ( nokori = rtp_payload_size - 12, ptr = recv_buf + 12; nokori > 0; nokori -= pcm_byte_per_frame, ptr+= pcm_byte_per_frame) {
+					p1 = history_bottom - ((SAMPLE_RATE/8) * 1);
+					p2 = history_bottom - ((SAMPLE_RATE/8) * 2);
+					p3 = history_bottom - ((SAMPLE_RATE/8) * 3);
+					p4 = history_bottom - ((SAMPLE_RATE/8) * 4);
+					p5 = history_bottom - ((SAMPLE_RATE/8) * 5);
+					p6 = history_bottom - ((SAMPLE_RATE/8) * 6);
+					if (p1 < 0) p1 += SAMPLE_RATE;
+					if (p2 < 0) p2 += SAMPLE_RATE;
+					if (p3 < 0) p3 += SAMPLE_RATE;
+					if (p4 < 0) p4 += SAMPLE_RATE;
+					if (p5 < 0) p5 += SAMPLE_RATE;
+					if (p6 < 0) p6 += SAMPLE_RATE;
 					left  = (*(ptr+0) << 8) | *(ptr+1);
 					right = (*(ptr+2) << 8) | *(ptr+3);
-					left  = left / 8;
-					right = right / 8;
+					// 16bit signed to 32bit signed
+					if (left & 0x8000)
+						left  |= 0xffff0000;
+					if (right & 0x8000)
+						right |= 0xffff0000;
+					history_left [ history_bottom ] = left;
+					history_right[ history_bottom ] = right;
+					left  = left + (history_left [p1]>>1) + (history_left [p2]>>2) + (history_left [p3]>>3) + (history_left [p4]>>4) + (history_left [p5]>>5) + (history_left [p6]>>6);
+					right = right+ (history_right[p1]>>1) + (history_right[p2]>>2) + (history_right[p3]>>3) + (history_right[p4]>>4) + (history_right[p5]>>5) + (history_right[p6]>>6);
 					*(ptr+0) = left >> 8;
 					*(ptr+1) = left & 0xff;
 					*(ptr+2) = right >> 8;
 					*(ptr+3) = right & 0xff;
+					if (++history_bottom >= SAMPLE_RATE)
+						history_bottom = 0;
 				}
 			}
 
 			if (pcm_byte_per_frame == 6) {
-				int left, right;
+				int left, right, p1, p2, p3, p4, p5, p6;
 				for ( nokori = rtp_payload_size - 12, ptr = recv_buf + 12; nokori > 0; nokori -= pcm_byte_per_frame, ptr+= pcm_byte_per_frame) {
+					p1 = history_bottom - ((SAMPLE_RATE/8) * 1);
+					p2 = history_bottom - ((SAMPLE_RATE/8) * 2);
+					p3 = history_bottom - ((SAMPLE_RATE/8) * 3);
+					p4 = history_bottom - ((SAMPLE_RATE/8) * 4);
+					p5 = history_bottom - ((SAMPLE_RATE/8) * 5);
+					p6 = history_bottom - ((SAMPLE_RATE/8) * 6);
+					if (p1 < 0) p1 += SAMPLE_RATE;
+					if (p2 < 0) p2 += SAMPLE_RATE;
+					if (p3 < 0) p3 += SAMPLE_RATE;
+					if (p4 < 0) p4 += SAMPLE_RATE;
+					if (p5 < 0) p5 += SAMPLE_RATE;
+					if (p6 < 0) p6 += SAMPLE_RATE;
 					left  = (*(ptr+0) << 16) | (*(ptr+1) << 8) | *(ptr+2);
 					right = (*(ptr+3) << 16) | (*(ptr+4) << 8) | *(ptr+5);
 					// 24bit signed to 32bit signed
 					if (left & 0x800000)
-						left |= 0xff000000;
+						left  |= 0xff000000;
 					if (right & 0x800000)
 						right |= 0xff000000;
-					left  = left / 8;
-					right = right / 8;
+					history_left [ history_bottom ] = left;
+					history_right[ history_bottom ] = right;
+					left  = left + (history_left [p1]>>1) + (history_left [p2]>>2) + (history_left [p3]>>3) + (history_left [p4]>>4) + (history_left [p5]>>5) + (history_left [p6]>>6);
+					right = right+ (history_right[p1]>>1) + (history_right[p2]>>2) + (history_right[p3]>>3) + (history_right[p4]>>4) + (history_right[p5]>>5) + (history_right[p6]>>6);
 					*(ptr+0) =  left  >> 16;
 					*(ptr+1) = (left  >> 8) & 0xff;
 					*(ptr+2) =  left  & 0xff;
 					*(ptr+3) =  right >> 16;
 					*(ptr+4) = (right >> 8) & 0xff;
 					*(ptr+5) =  right & 0xff;
+					if (++history_bottom >= SAMPLE_RATE)
+						history_bottom = 0;
 				}
 			}
 
